dc96ebf7054b3560c60eadccb3178e51f1d46aad
FrankYe <frankye@FrankYes-iMac.local>
2018-12-08 21:59:07 +0800

    shortest path distance in Djastra

--- src/greedy_algorithm/single_source_shortest_path/SingleSoutceShortestPath.java
+++ src/greedy_algorithm/single_source_shortest_path/SingleSoutceShortestPath.java
@@ -1,21 +1,55 @@
 package greedy_algorithm.single_source_shortest_path;
 
+import java.util.HashSet;
+import java.util.Set;
+
 public class SingleSoutceShortestPath {
 	public static final Double NaN = Double.NaN;
-	public double getMinValue(double[] distance) {
-		double value = distance[0];
+	
+	public int getMinValueLargeThanZero(double[] distance) {
+		int value = Integer.MAX_VALUE;
 
-		for(int i=1; i<distance.length; i++) {
-			if(distance[i] < value) {
-				value = distance[i];
+		for(int i=0; i<distance.length; i++) {
+			if(distance[i] < value && distance[i]>0) {
+				value = i;
 			}
 		}
 
 		return value;
 	}
 
+	public Set<Integer> getUnicomDistanceIndex(double[] distance) {
+		Set<Integer> unicomDistance = new HashSet<Integer>();
+
+		for (int i=0; i<distance.length; i++) {
+			if(distance[i]>0) {
+				unicomDistance.add(i);
+			}
+		}
+		
+		return unicomDistance;
+	}
+
+	public void updateDistance(double[] distance, Set<Integer> unicomDistanceIndex, int current_index, int shortest_path_index, double[][] distanceMap) {
+		for (int idx: unicomDistanceIndex) {
+			System.out.println(idx);
+//			if (distanceMap[current_index][idx] == NaN) {
+//				distance[idx] = distanceMap[current_index][idx];
+//			}
+//			else {
+//				distance[idx] = Math.min(distanceMap[shortest_path_index][idx]+distanceMap[current_index][shortest_path_index], distanceMap[current_index][idx]);
+//			}
+		}
+		System.out.print("###");
+		
+	}
+
+
+
 	public static void main(String[] args) {
-		double[][] e = {
+		SingleSoutceShortestPath shortestPath = new SingleSoutceShortestPath();
+
+		double[][] distanceMap = {
 				{0, 1, 12, NaN, NaN, NaN},
 				{NaN, 0, 9, 3, NaN, NaN},
 				{NaN, NaN, 0, NaN, 5, NaN},
@@ -24,9 +58,21 @@ public class SingleSoutceShortestPath {
 				{NaN, NaN, NaN, NaN, NaN, 0},
 		};
 
-		double[] distance = {0, 1, 12, NaN, NaN, NaN};
+		double[] distance = distanceMap[0];
 
+		for (int i=0; i<distanceMap[0].length; i++) {
+//			double[] distance = distanceMap[i];
+			int shortest_path_index = shortestPath.getMinValueLargeThanZero(distance);
 
+			Set<Integer> unicomDistanceSet = shortestPath.getUnicomDistanceIndex(distanceMap[shortest_path_index]);
+
+			shortestPath.updateDistance(distance, unicomDistanceSet, i, shortest_path_index, distanceMap);
+
+		}
+
+		for (int i=0; i<distance.length; i++) {
+			System.out.println(distance[i]);
+		}
 
 	}
 }
